86b92ffd3523f96eb38a9d74e20eb9d39a3cc1e5
ElijahPatric <foreverelijah@gmail.com>
2020-02-06 21:16:32 -0600

    Initial Commit

---
+++ BugTracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BugTracker
+//
+//  Created by Elijah on 2/6/20.
+//  Copyright © 2020 Elijah Patric. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
